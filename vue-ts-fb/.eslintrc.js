211073385dfaa7a49d3c50a8ec0446fda944c64e
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-07-05 23:40:11 +0900

    eslint 설정 변경

--- vue-ts-fb/.eslintrc.js
+++ vue-ts-fb/.eslintrc.js
@@ -4,15 +4,17 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    "plugin:vue/essential",
+    "@vue/standard",
+    "@vue/typescript/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: ["off", "single"],
+    semi: 0
   }
-}
+};
