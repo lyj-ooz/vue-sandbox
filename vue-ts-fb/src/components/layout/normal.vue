99380d34fa2de780e92ace4cf390d7e89065f9ba
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-07-05 23:41:05 +0900

    컴포넌트 추가

---
+++ vue-ts-fb/src/components/layout/normal.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>layout normal</v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class LayoutNormal extends Vue {}
+</script>
+
+<style scoped></style>
